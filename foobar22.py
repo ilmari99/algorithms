9b0a245d866cced2a7d665df4805e6d86f82c2cb
ilmari99 <i.vahteristo@gmail.com>
2022-08-25 11:21:20 +0300

    Added text

--- foobar22.py
+++ foobar22.py
@@ -1,4 +1,27 @@
 """
+You need to pass a message to the bunny anycodings_python prisoners,
+but to avoid detection, the code anycodings_python you agreed to use is...
+obscure, to say the least. The bunnies are given food on
+ standard-issue prison plates that are anycodings_python
+stamped with the numbers 0-9 for easier sorting, and you
+need to combine sets of plates to create the numbers in the code.
+The signal that a number is part of the code is that it is divisible by 3. 
+You can do anycodings_python smaller numbers like 15 and 45 easily, but bigger numbers
+like 144 and 414 are a little anycodings_python trickier. Write a program to help yourself
+quickly create large numbers for use in the anycodings_python code, given a limited number of plates towork with.
+
+You have L, a list containing some digits (0 to 9). Write a function answer(L) 
+which finds the largest number that can be made anycodings_python from some or
+all of these digits and is divisible by 3. If it is not possible
+to anycodings_python make such a number, return 0 as the answer. L will contain
+anywhere from 1 to 9 digits. The same digit may appear multiple
+times in the list, but each element in the list may only be used once.
+
+
+ME
+==
+This wasn't that hard after figuring out that a number is divisible by 3 if the digits sum is divisible by 3.
+
 Returns the largest integer divisible by 3 that can be arranged from the input list L containing single digits.
 USAGE: 
 `python3 foobar22.py <n1> <n2> <n3> .... <nk>`
