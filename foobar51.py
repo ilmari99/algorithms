b9f16c1577bc4ebd09dab0085eee735a45d17735
ilmari99 <i.vahteristo@gmail.com>
2022-08-25 11:23:43 +0300

    Added solution

--- foobar51.py
+++ foobar51.py
@@ -1,201 +1,202 @@
-import itertools as it
+"""
+Disorderly Escape
+=================
+Oh no! You've managed to free the bunny prisoners and escape Commander Lambdas
+exploding space station, but her team of elite starfighters has flanked your ship.
+If you dont jump to hyperspace, and fast, youll be shot out of the sky!
+Problem is, to avoid detection by galactic law enforcement, Commander Lambda planted
+her space station in the middle of a quasar quantum flux field. In order to make the
+jump to hyperspace, you need to know the configuration of celestial bodies in the quadrant
+you plan to jump through. In order to do *that*, you need to figure out how many configurations
+each quadrant could possibly have, so that you can pick the optimal quadrant through which
+youll make your jump. 
+
+There's something important to note about quasar quantum flux fields' configurations: when
+drawn on a star grid, configurations are considered equivalent by grouping rather than by
+order. That is, for a given set of configurations, if you exchange the position of any two
+columns or any two rows some number of times, youll find that all of those configurations are
+equivalent in that way - in grouping, rather than order.
+
+Write a function answer(w, h, s) that takes 3 integers and returns the number of unique,
+non-equivalent configurations that can be found on a star grid w blocks wide and h blocks
+tall where each celestial body has s possible states. Equivalency is defined as above: any
+two star grids with each celestial body in the same state where the actual order of the rows
+and columns do not matter (and can thus be freely swapped around). Star grid standardization 
+means that the width and height of the grid will always be between 1 and 12, inclusive. And while
+there are a variety of celestial bodies in each grid, the number of states of those bodies is 
+between 2 and 20, inclusive. The answer can be over 20 digits long, so return it as a decimal 
+string.  The intermediate values can also be large, so you will likely need to use at least 64-bit integers.
+
+For example, consider w=2, h=2, s=2. We have a 2x2 grid where each celestial body is either
+in state 0 (for instance, silent) or state 1 (for instance, noisy).  We can examine which 
+grids are equivalent by swapping rows and columns.
+00
+00
+In the above configuration, all celestial bodies are "silent" - that is, they have a 
+state of 0 - so any swap of row or column would keep it in the same state.
+00 00 01 10
+01 10 00 00
+1 celestial body is emitting noise - that is, has a state of 1 - so swapping rows and columns 
+can put it in any of the 4 positions.  All four of the above configurations are equivalent.
+00 11
+11 00
+2 celestial bodies are emitting noise side-by-side.  Swapping columns leaves them unchanged, 
+and swapping rows simply moves them between the top and bottom.  In both, the *groupings* are 
+the same: one row with two bodies in state 0, one row with two bodies in state 1, and two columns with one of each state.
+01 10
+01 10
+2 noisy celestial bodies adjacent vertically. This is symmetric to the side-by-side case, but 
+it is different because there's no way to transpose the grid.
+01 10
+10 01
+2 noisy celestial bodies diagonally.  Both have 2 rows and 2 columns that have one of each 
+state, so they are equivalent to each other.
+01 10 11 11
+11 11 01 10
+3 noisy celestial bodies, similar to the case where only one of four is noisy.
+11
+11
+4 noisy celestial bodies.
+There are 7 distinct, non-equivalent grids in total, so answer(2, 2, 2) would return 7.
+
+Test cases
+==========
+Inputs:
+    (int) w = 2
+    (int) h = 2
+    (int) s = 2
+Output:
+    (string) "7"
+Inputs:
+    (int) w = 2
+    (int) h = 3
+    (int) s = 4
+Output:
+    (string) "430"
+
+
+
+ME
+==
+This assignment truly required a lot of work to get the correct answer.
+I had to look at some explanations for the problem, and I still can't say that this would've been easy. Or that
+I would completely understand some of the equations.
+
+A great introduction to abstract algebra and group theory, symmetries and some damn cycle indices!
+
+The nudge in the right direction came after learning about (not) Burnsides's Lemma, which states that the number of equivalency classes
+(the number of orbits, number of non-equivalent matrices) equals the average number of elements in X (all matrices of size w*h) that
+are fixed by g in the action group G.
+
+After implementing it I realized it wasn't nearly efficient enough with big-o of something like O(w!h!). 
+Furthermore, then I didn't know it produced correct answers outside simple cases as I had no way to test.
+
+After the implementation of Burnsides Lemma, I was desperate and quickly lost in the lingo of group theory, symmetry groups, and
+abstract algebra. I realized the problem was beyond my reach and just had to look up discussions about the problem (on Stackoverflow obviously).
+
+The solution involved a lot of math I was very unfamiliar with, and even reading solutions led me atleast 5 research tangents deep in abstract algebra.
+
+After many painstaking hours (or days, who knows when time flies) and gradually lowering my standards as to how much I
+should understand about the solution before daring to submit it, I got the implementation correct.
+I didn't understand how some of the facts came to being, but I understood enough to be very very happy about reaching a solution.
+
+This was the last challenge in the series, and I'm proud of completing all the challenges and being thrown deep into the world of algorithms.
+"""
+
+
 import math
-import numpy as np
+from fractions import Fraction, gcd
+from collections import Counter
 
-def _combinations(sample_len, els):
-    """ Calculate the combinations (order doesn't matter) with replacement.
-    Picks sample_len elements from els.
-    
-    els : List of elements to pick from OR number of elements to pick from (converts to range(els))
-    sample_len : Number of elements to pick from els
-    
-    Returns:
-        a tuple of tuples of the combinations of sample_len elements from els elements with replacement.
+def solution(w,h,s):
     """
-    if isinstance(els,int):
-        els = range(0,els)
-    return tuple(it.combinations_with_replacement(els,sample_len))
-
-def _permutations(sample_len, els):
-    """ Calculate the permutations (order matters) with replacement.
-    Picks sample_len elements from els.
+    Return the number of non-equivalent (https://en.wikipedia.org/wiki/Matrix_equivalence) 
+    matrices with dimensions w x h, and s different possible values.
     
-    els : List of elements to pick from OR number of elements to pick from (converts to range(els))
-    sample_len : Number of elements to pick from els
+    This is the same as the number of orbits (https://en.wikipedia.org/wiki/Group_action#Orbits_and_stabilizers)
+    for the action group G (all different row and column switches)
+    acting on set X (all different matrices filling the restrictions).
     
-    Returns:
-        a tuple of tuples of the permutations of sample_len elements from els elements with replacement.
+    This in turn is the same as the average number of fixed points (https://en.wikipedia.org/wiki/Group_action#Fixed_points_and_stabilizer_subgroups)
+    for the action group G acting on set X.
     """
-    if isinstance(els,int):
-        els = range(0,els)
-    return tuple(it.permutations(els,sample_len))
+    ans = 0
+    row_cycle_vectors = get_cycle_vectors(h)    # Get the cycle vectors and their coefficients
+    col_cycle_vectors = get_cycle_vectors(w)
+    # Loop through all row and column cycles
+    for rc_vec,r_coef in row_cycle_vectors.items():
+        for cc_vec,c_coef in col_cycle_vectors.items():
+            coeff = r_coef*c_coef
+            combined_vec = combine(rc_vec,cc_vec)
+            value = 1
+            # Combine the cycle indices and calculate cycle index of the cartesian product of the cycles
+            for _, power in combined_vec:
+                value *= s ** power
+            ans += coeff * value
+    return str(int(ans))
 
-def number_of_permutations(sample_len):
-    return math.factorial(sample_len)
-
-def number_of_combinations(sample_len,els):
-    """Return the number of combinations of sample_len elements from els elements with replacement.
-    Args:
-        n_el (_type_): number of elements to pick from
-        sample_len (_type_): Number of elements to pick from els
-
-    Returns:
-        integer : number of combinations of sample_len elements from els elements with replacement.
+def combine(a_cycles, b_cycles):
     """
-    if not isinstance(els,(int,float)):
-        els = len(els)
-    #https://www.calculatorsoup.com/calculators/discretemathematics/combinationsreplacement.php
-    return math.factorial(els + sample_len - 1) / (math.factorial(sample_len)*math.factorial(els - 1))
-
-def _non_equiv_permutation_matrices(M,compare_row=0):
-    if compare_row >= M.shape[0]-1:
-        yield M
+    Combine the row cycles and the column cycles according to this formula:
+    https://math.stackexchange.com/questions/2113657/burnsides-lemma-applied-to-grids-with-interchanging-rows-and-columns/2343500#2343500.
+    len_ tells the length of the cycle
+    freg_ tells the frequency of len_ subcycles in _cycles
+    """
+    combined = []
+    for len_a, freq_a in enumerate(a_cycles):
+        len_a += 1      # Add one because len_ != 0
+        for len_b, freq_b in enumerate(b_cycles):
+            len_b += 1
+            lcm = (len_a * len_b) / gcd(len_a, len_b)   # Calculate least-common multiple
+            combined.append((lcm, int(len_a * freq_a * len_b * freq_b / lcm)))
+    return combined
+    
+def partitions(n):
+    """
+    Recursively yield the partitions of n as a list of the components (each list sums to n).
+    """
+	# base case
+    if n == 0:
+        yield []
         return
-    M = M.copy()
-    same_row_count = sum([True if np.array_equal(M[compare_row],M[i]) and i != compare_row else False for i in range(0,M.shape[0])])
-    unique_values_index = np.unique(M[compare_row],return_index=True)[1]
-    unique_values_count = len(unique_values_index)
-    #perm_count = 0
-    permutated = {}
-    matrices = []
-    if same_row_count > 0 and unique_values_count > 1:
-        print("********************************************************")
-        print("Matrix: {}".format(M))
-        #print("Row: {} can be permutated to".format(M[0]))
-        for p in _permutations(len(M[compare_row]),M[compare_row]):
-            M[compare_row] = p
-            if tuple(M[compare_row]) in permutated:
-                continue
-            permutated[tuple(M[compare_row])] = True
-            for m in _non_equiv_permutation_matrices(M,compare_row+1):
-                yield m
-            #perm_count += len(_permutations(len(M[0]),M[0])) - 1
-            #print("Permutation {}\n: {}".format(perm_count,M))
-        #print("********************************************************")
-        #c = number_of_permutations(unique_values)*same_row_count
-    #return perm_count
-    
-def _non_equiv_permutation_matrices(M,compare_row=0):
-    if compare_row >= M.shape[0]-1:
-        #yield M
-        return 0
-    M = M.copy()
-    same_row_count = sum([True if np.array_equal(M[compare_row],M[i]) and i != compare_row else False for i in range(0,M.shape[0])])
-    unique_values_index = np.unique(M[compare_row],return_index=True)[1]
-    unique_values_count = len(unique_values_index)
-    #permutated = {tuple(M[compare_row]):True}
-    permutated = {}
-    M_count = 0
-    if unique_values_count > 1:
-        print("********************************************************")
-        print("Matrix: {}".format(M))
-        #print("Row: {} can be permutated to".format(M[0]))
-        for p in _permutations(len(M[compare_row]),M[compare_row]):
-            M[compare_row] = p
-            if tuple(M[compare_row]) in permutated:
-                continue
-            permutated[tuple(M[compare_row])] = True
-            M_count += number_of_permutations(len(M[compare_row])) - 1
-    return M_count
+    # get partitions of n-1
+    for p in partitions(n-1):
+        yield [1] + p
+        if p and (len(p) < 2 or p[1] > p[0]):
+            yield [p[0] + 1] + p[1:]
 
-def solution(w,h,s):
-    """_summary_
-
-    Args:
-        w : width of grid (number of columns)
-        h : height of grid (number of rows)
-        s : number of distinct possible values in grid
+def get_cycle_vectors(n):
     """
-    combs = _combinations(w,s) # distinct rows of length w with atmost s distinct values
-    print("Row combinations",combs)
+    https://en.wikipedia.org/wiki/Cycle_index#Disjoint_cycle_representation_of_permutations
+    Return the unique cycles of an array of length n (conjugacy classes, which are partitions of integer n),
+    and the corresponding coefficients for the cycle index.
+    Returned as a dictionary with unique_cycle : coefficient -pairs.
+    The cycles are encoded so that, the index(+1) denotes the length of the subcycle
+    and the value at the index(+1) denotes the number of such type cycles (same length).
+    Kind of like one-hot encoded vectors.
     
-    n_rows = number_of_combinations(w,s)    #Number of distinct rows of lenght w with s distinct values
-    print("Number of rows: {}".format(n_rows))
-    
-    print("Matrices")
-    solutions = _combinations(h,combs) # Base matrices: distinct matrices of height h with rows of lenght w with s distinct values
-    #n_sols = number_of_combinations(h,n_rows)
-    n_sols = 0
-    print("Len of solutions: {}".format(len(solutions)))
-    # All base matrices are non-equivalent
-    for sol in solutions:
-        #for a in _non_equiv_permutation_matrices(np.array(sol)):
-        print(sol)
-        for rn in range(0,h):
-            n_sols += _non_equiv_permutation_matrices(np.array(sol[rn:]),rn)
-            #print(a,"\n")
-    return str(int(n_sols))
-
-def ch_order(a,row_order,col_order):
-    """_summary_
-
-    Args:
-        a : matrix to reorder
-        row_order : order of rows (indexes)
-        col_order : order of columns (indexes)
+    Each member in the dictionary then essentially holds the dummy variable of a cycle of disctinct length (the cycle indexes term) and its coefficient.
     """
-    a_cp = a.copy()
-    a = a_cp[row_order,:]
-    a = a[:,col_order]
-    return a
+    vectors = partitions(n) # Get the distinct cycles (conjugacy classes), which are the partitions: https://en.wikipedia.org/wiki/Symmetric_group#Conjugacy_classes
+    J = {}
+    for v in vectors:
+        base = [0 for _ in range(n)]
+        c = Counter(v)
+        for item, count in c.items():
+            base[item-1] += count   # Modfiy base vector, leaving 0 as val
+        J[tuple(base)] = coefficient_of_cycle(base)
+    return J
 
-def loops_in_matrix(M,ret_loops=False):
-    loops = []
-    in_loop = {}
-    m = M.flatten()
-    loop_count = 0
-    for i,el in enumerate(m):
-        if i in in_loop:
-                continue
-        in_loop[i] = True
-        while i != el:
-            in_loop[el] = True
-            el = m[el]
-        loop_count += 1
-    return loop_count
-            
-            
-def create_array(w,h):
-    a = np.ones((h,w),dtype=int)
-    for i in range(0,h):
-        for j in range(0,w):
-            a[i,j] = i*w+j
-    return a
+def coefficient_of_cycle(j):
+    """
+    Return the coefficient of a cycle j according to a formula for counting it.
+    It is the inner part of the formula for the cycle index, found here: https://en.wikipedia.org/wiki/Cycle_index#Symmetric_group_Sn
+    Returns it as Fraction instance for higher accuracy.
+    """
+    # in j, index + 1 is the length of the subcycle and the value at index is the number of times the subcycle is repeated in the cycle.
+    s = 1
+    for n in range(1,len(j)+1):
+        s *= math.factorial(j[n-1])*n**j[n-1]
+    return Fraction(1,s)
 
-def solution(w,h,s):
-    b_arr = create_array(w,h)   #Base array: array of size hxw with elements 0,1,2,...,hxw-1
-    row_orders = _permutations(h,h)
-    col_orders = _permutations(w,w)
-    sums = 0
-    transformations = 0
-    for row_order in row_orders:
-        for col_order in col_orders:
-            a = ch_order(b_arr,row_order,col_order)     # Change the base_matrix
-            transformations += 1
-            loops = loops_in_matrix(a)
-            sums += s**loops
-    return str(int(float(sums)/float(transformations)))
 
-def test_loops_in_matrix():
-    cases = {
-        1:create_array(1,1),
-        4:create_array(2,2),
-        3:np.array([[1,0],[5,4],[3,2]]),
-            }
-    for m in cases:
-        assert loops_in_matrix(cases[m]) == m
-
-def test_create_array():
-    cases = [
-        (np.array([[0,1],[2,3]]),(2,2)),
-        (np.array([[0,1],[2,3],[4,5]]),(2,3)),
-    ]
-    for m in cases:
-        assert np.array_equal(m[0], create_array(*m[1]))
-
-if __name__ == "__main__":
-    test_loops_in_matrix()
-    test_create_array()
-    case = (1,2,2)
-    ans = solution(*case)
-    print("Answer:",ans)
